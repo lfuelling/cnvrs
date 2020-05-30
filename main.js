42ad0eae82dfd11000ea10a9750415af05417414
Lukas Fülling <lukas@k40s.net>
2020-05-30 15:16:34 +0200

    remove parcel, fix libsignal

--- main.js
+++ main.js
@@ -4,49 +4,32 @@ const path = require("path");
 const isDev = require("electron-is-dev");
 
 function createWindow () {
-    // Erstelle das Browser-Fenster.
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1150,
+        height: 800,
+        devTools: isDev,
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: false
         }
     })
 
-    win.loadURL(
-        isDev
-            ? "http://localhost:3000"
-            : `file://${path.join(__dirname, "../build/index.html")}`
-    );
+    win.loadFile('index.html');
 
     if(isDev) {
-        // Öffnen der DevTools.
-        win.webContents.openDevTools();
+        win.webContents.openDevTools({mode: "detach"});
     }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Einige APIs können nur nach dem Auftreten dieses Events genutzt werden.
 app.whenReady().then(createWindow)
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-    // Unter macOS ist es üblich, für Apps und ihre Menu Bar
-    // aktiv zu bleiben, bis der Nutzer explizit mit Cmd + Q die App beendet.
     if (process.platform !== 'darwin') {
         app.quit()
     }
 })
 
 app.on('activate', () => {
-    // Unter macOS ist es üblich ein neues Fenster der App zu erstellen, wenn
-    // das Dock Icon angeklickt wird und keine anderen Fenster offen sind.
     if (BrowserWindow.getAllWindows().length === 0) {
         createWindow()
     }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. Sie können den Code auch
-// auf mehrere Dateien aufteilen und diese hier einbinden.
